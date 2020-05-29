048727b4a06c48a7bbb08811132ce01059d917ef
Karim Naguib <karimn2.0@gmail.com>
2020-05-29 14:51:32 -0400

    Model successfully running

--- mobility-model/mobility.stan
+++ mobility-model/mobility.stan
@@ -1,5 +1,14 @@
 functions {
+  int[] add_array(int[] arr, int to_add) {
+    int arr_size = num_elements(arr);
+    int result[arr_size] = arr;
 
+    for (arr_index in 1:arr_size) {
+      result[arr_index] += to_add;
+    }
+
+    return result;
+  }
 }
 
 data {
@@ -8,39 +17,46 @@ data {
   int<lower = 1> N_national; // Number of countries
   int<lower = 1> N_subnational[N_national]; // Number of subnational entities for each country
 
+  int<lower = 1> start_epidemic_offset;
+
   int<lower = 1> days_observed[sum(N_subnational)]; // Days observed per national/subnational entity can be different
   int<lower = 1, upper = min(days_observed)> days_to_impute_cases; // Same for all national/subnational entities. We can change that to vary if needed.
-  int<lower = max(days_observed)> total_days; // days observed + days to predict to
+  int<lower = 0> days_to_forecast;
 
-  vector<lower = 0>[max(days_observed)] time_to_death; // This is pi in the Vollmer model. Needs to be pre-computed empirically using ecdf(); it's the sum of two gamma distributions
+  vector<lower = 0>[max(days_observed) + days_to_forecast] time_to_death; // This is pi in the Vollmer model. Needs to be pre-computed empirically using ecdf(); it's the sum of two gamma distributions
 
   int<lower = 0> num_coef; // Number of coefficients for linear model
-
-  // Below is the data needed
+  matrix[sum(days_observed) + sum(N_subnational) * days_to_forecast, num_coef] design_matrix; // E.g., mobility
 
   int<lower = 0> population[sum(N_subnational)];
   vector<lower = 0>[sum(N_subnational)] mean_ifr; // Not sure we'd have this at the subnational level. If we don't we change this to the national level.
-  matrix[total_days, num_coef] design_matrix[sum(N_subnational)]; // E.g., mobility
   int<lower = 0> deaths[fit_model ? sum(days_observed) : 0];
 }
 
 transformed data {
   int N = sum(N_subnational);
   int D = sum(days_observed);
-  int D_total = N * total_days;
+  int total_days[N] = add_array(days_observed, days_to_forecast);
+  int D_total = sum(total_days);
   int max_days_observed = max(days_observed);
 
-  real gen_factor_alpha = 6.5;
-  real gen_factor_beta = 0.62;
+  real gen_factor_alpha = 1 / (0.62^2);          // alpha = 1 / tau^2;
+  real gen_factor_beta = gen_factor_alpha / 6.5;     // beta = 1 / (tau^2 * mu)
 
   // Need to discretize
   vector<lower = 0>[max_days_observed] gen_factor;
 
-  gen_factor[1] = gamma_cdf(1.5, gen_factor_alpha, gen_factor_beta);
+  {
+    vector[max_days_observed] gamma_cdfs;
 
-  // Discretization
-  for (day_index in 2:max_days_observed) {
-    gen_factor[day_index] = gamma_cdf(day_index + 0.5, gen_factor_alpha, gen_factor_beta) - gen_factor[day_index - 1];
+    gamma_cdfs[1] = gamma_cdf(1.5, gen_factor_alpha, gen_factor_beta);
+    gen_factor[1] = gamma_cdfs[1];
+
+    // Discretization
+    for (day_index in 2:max_days_observed) {
+      gamma_cdfs[day_index] = gamma_cdf(day_index + 0.5, gen_factor_alpha, gen_factor_beta);
+      gen_factor[day_index] = gamma_cdfs[day_index] - gamma_cdfs[day_index - 1];
+    }
   }
 }
 
@@ -52,9 +68,11 @@ parameters {
 
   // Linear model parameters
   vector[num_coef] beta_toplevel;
+
   vector<lower = 0>[num_coef] beta_national_sd;
   matrix[num_coef, N_national] beta_national_raw; // Uncentered for now to avoid divergence
-  matrix<lower = 0>[N_national, num_coef] beta_subnational_sd;
+
+  matrix<lower = 0>[num_coef, N_national] beta_subnational_sd;
   matrix[num_coef, N] beta_subnational_raw;
 
   vector<lower = 0>[N] ifr_noise;
@@ -64,12 +82,13 @@ parameters {
 }
 
 transformed parameters {
-  matrix<lower = 0>[total_days, N] mean_deaths; // Not a matrix; this could be a ragged data structure
-  matrix<lower = 0>[total_days, N] Rt = rep_matrix(0, total_days, N);
-  matrix<lower = 0>[N, total_days] new_cases = rep_matrix(0, N, total_days);
+  vector<lower = 0>[D_total] mean_deaths; // Not a matrix; this could be a ragged data structure
+  vector<lower = 0>[D_total] Rt = rep_vector(0, D_total);
+  row_vector<lower = 0>[D_total] new_cases = rep_row_vector(0, D_total);
 
   {
     int subnat_pos = 1;
+    int days_pos = 1;
 
     vector[N] ifr = mean_ifr .* ifr_noise;
 
@@ -83,25 +102,50 @@ transformed parameters {
 
       for (subnat_index in 1:N_subnational[country_index]) {
         int curr_subnat_pos = subnat_pos + subnat_index - 1;
+        int days_end = days_pos + days_observed[curr_subnat_pos] - 1;
 
-        vector[total_days] cumulative_cases;
+        vector[total_days[curr_subnat_pos]] cumulative_cases;
+
+        // print("subnat_index = ", subnat_index);
 
         beta[, curr_subnat_pos] += beta_subnational_raw[, curr_subnat_pos] .* beta_subnational_sd[, country_index];
 
-        new_cases[curr_subnat_pos] = rep_row_vector(imputed_cases[subnat_index], days_to_impute_cases);
+        new_cases[days_pos:(days_pos + days_to_impute_cases - 1)] = rep_row_vector(imputed_cases[subnat_index], days_to_impute_cases);
 
-        Rt[, curr_subnat_pos] = 2 * R0[curr_subnat_pos] * inv_logit(design_matrix[curr_subnat_pos] * beta[, curr_subnat_pos]);
+        Rt[days_pos:days_end] = 2 * R0[curr_subnat_pos] * inv_logit(design_matrix[days_pos:days_end] * beta[, curr_subnat_pos]);
 
-        for (day_index in 1:total_days) {
+        for (day_index in 1:total_days[curr_subnat_pos]) {
+          int curr_day_pos = days_pos + day_index - 1;
+
           if (day_index > days_to_impute_cases) {
             real adjust_factor = 1 - (cumulative_cases[day_index - 1] / population[curr_subnat_pos]);
 
-            new_cases[curr_subnat_pos, day_index] = adjust_factor * Rt[day_index, curr_subnat_pos] * (new_cases[curr_subnat_pos, 1:(day_index - 1)] * gen_factor[1:(day_index - 1)]);
+            // print("adjust_factor[", day_index , "] = ", adjust_factor);
+            // print("Rt[", day_index, "] = ", Rt[curr_day_pos]);
+            // print("new_cases[:] = ", new_cases[days_pos:(curr_day_pos - 1)]);
+            // print("gen_factor[:] = ", gen_factor[1:(day_index - 1)]);
+
+            new_cases[curr_day_pos] = adjust_factor * Rt[curr_day_pos] * new_cases[days_pos:(curr_day_pos - 1)] * gen_factor[1:(day_index - 1)];
           }
 
-          cumulative_cases[day_index] = new_cases[curr_subnat_pos, day_index] + (day_index > 1 ? cumulative_cases[day_index - 1] : 0);
-          mean_deaths[day_index, curr_subnat_pos] = ifr[curr_subnat_pos] * (new_cases[curr_subnat_pos, 1:(day_index - 1)] * time_to_death[1:(day_index - 1)]);
+          // print("new_cases[", day_index, "] = ", new_cases[curr_day_pos]);
+
+          cumulative_cases[day_index] = new_cases[curr_day_pos] + (day_index > 1 ? cumulative_cases[day_index - 1] : 0);
+
+          if (day_index > 1) {
+            mean_deaths[curr_day_pos] = ifr[curr_subnat_pos] * new_cases[days_pos:(curr_day_pos - 1)] * time_to_death[1:(day_index - 1)];
+          } else {
+            mean_deaths[curr_day_pos] = 1e-15 * new_cases[curr_day_pos];
+          }
+
+          if (mean_deaths[curr_day_pos] < 0) {
+            // print("mean_deaths[", curr_day_pos, "] = ", mean_deaths[curr_day_pos]);
+            // print("day_index = ", day_index);
+            // print("new_cases = ", new_cases[days_pos:(curr_day_pos - 1)]);
+          }
         }
+
+        days_pos = days_end + 1;
       }
 
       subnat_pos = subnat_end + 1;
@@ -115,7 +159,7 @@ model {
   ifr_noise ~ normal(1, 0.1);
 
   tau_impute_cases ~ exponential(0.03);
-  imputed_cases ~ exponential(1 / tau_impute_cases);
+  imputed_cases ~ exponential(1 / tau_impute_cases); // TODO imputed cases should vary by subnational unit
 
   beta_toplevel ~ normal(0, 0.5);
   beta_national_sd ~ normal(0, 0.5);
@@ -138,7 +182,7 @@ model {
         int curr_subnat_pos = subnat_pos + subnat_index - 1;
         int days_end = days_pos + days_observed[curr_subnat_pos] - 1;
 
-        deaths[days_pos:days_end] ~ neg_binomial_2(mean_deaths[1:days_observed[curr_subnat_pos], curr_subnat_pos], overdisp_deaths[curr_subnat_pos]);
+        deaths[(days_pos + start_epidemic_offset - 1):days_end] ~ neg_binomial_2(mean_deaths[(days_pos + start_epidemic_offset - 1):days_end], overdisp_deaths[curr_subnat_pos]);
 
         days_pos = days_end + 1;
       }
@@ -149,5 +193,5 @@ model {
 }
 
 generated quantities {
-
+  // Forecasting
 }
--- mobility-model/run_mob.R
+++ mobility-model/run_mob.R
@@ -5,6 +5,18 @@ library(tidyverse)
 library(rstan)
 library(wpp2019)
 
+options(mc.cores = max(1, parallel::detectCores()))
+rstan_options(auto_write = TRUE)
+
+# Constants ---------------------------------------------------------------
+
+min_deaths_day_before_epidemic <- 10
+seeding_days_before_epidemic <- 30
+start_epidemic_offset <- seeding_days_before_epidemic + 1
+days_seeding <- 6
+days_to_forecast <- 0
+mob_formula <- ~ 0 + g_residential + g_transit_stations + average_mob
+
 # Population Data --------------------------------------------------------
 
 data(pop)
@@ -57,44 +69,6 @@ wb_pop_data <- read_csv(file.path("data", "population", "wb_subnational_pop.csv"
   ungroup() %>%
   select(-year)
 
-
-# Subnational Mobility Data -----------------------------------------------
-
-subnat_mob_data <- haven::read_dta(file.path("~", "Dropbox", "COVID-19", "Data Collection", "Travel", "clean", "google_trends.dta")) %>%
-  select(countrycode_string, sub_region, date, starts_with("g_")) %>%
-  mutate(average_mob = (g_grocery_pharma + g_parks + g_retail_recreation + g_workplaces) / 4) %>%
-  group_nest(countrycode_string, sub_region, .key = "daily_data") %>%
-  mutate(
-    first_day_mob = map(daily_data, pull, date) %>% map_dbl(min),
-    last_day_mob = map(daily_data, pull, date) %>% map_dbl(max)
-  ) %>%
-  mutate_at(vars(first_day_mob, last_day_mob), lubridate::as_date)
-
-# Subnational Deaths Data -------------------------------------------------
-
-subnat_data <- haven::read_dta(file.path("~", "Dropbox", "COVID-19", "Analysis", "Data", "subspread.dta")) %>%
-  left_join(transmute(countrycode::codelist, countrycode_string = iso3c, countrycode = iso3n), by = "countrycode_string") %>%
-  group_nest(countrycode, countrycode_string, sub_region, .key = "daily_data") %>%
-  inner_join(subnat_mob_data, by = c("countrycode_string", "sub_region"), suffix = c("_spread", "_mob")) %>%
-  mutate(
-    first_day_deaths = map(daily_data_spread, pull, date) %>% map_dbl(min, na.rm = TRUE) %>% lubridate::as_date(), # TODO There shouldn't be any NA dates
-    daily_data_spread = pmap(lst(daily_data_spread, first_day_mob, last_day_mob), ~ filter(..1, between(date, ..2, ..3))),
-    daily_data_mob = pmap(lst(daily_data_mob, first_day_deaths), ~ filter(..1, date >= ..2)),
-    daily_data = map2(daily_data_spread, daily_data_mob, right_join, by = "date")
-  ) %>%
-  select(-daily_data_mob, -daily_data_spread) %>%
-  filter(map_int(daily_data, nrow) > 0) %>% # Only entities with any observations
-  mutate(
-    first_day = map(daily_data, pull, date) %>% map_dbl(min, na.rm = TRUE) %>% lubridate::as_date(),
-    last_day = map(daily_data, pull, date) %>% map_dbl(max, na.rm = TRUE) %>% lubridate::as_date(),
-    daily_data = pmap(lst(daily_data, first_day, last_day), ~ complete(..1, date = seq.Date(from = ..2, to = ..3, by = "day"))),
-    days_observed = map_int(daily_data, nrow),
-    num_missing_new_deaths = map(daily_data, filter, is.na(new_deaths)) %>% map_int(nrow),
-    num_missing_mob = map(daily_data, select, starts_with("g_")) %>% map(map, is.na) %>% map(reduce, ~ .x | .y) %>% map_int(sum)
-    ) %>%
-  arrange(countrycode, sub_region) %>%
-  left_join(wb_pop_data, by = c("countrycode_string", "sub_region")) # Only available for regions (not all) in Brazil, India, and China
-
 # IFR ---------------------------------------------------------------------
 
 age_metadata <- age_conv %>%
@@ -107,10 +81,80 @@ age_metadata <- age_conv %>%
 ifr_adj <- pop_data %>%
   filter(fct_match(year, "2020")) %>%
   left_join(age_metadata, by = "age_group") %>%
-  select(-old_age_group) %>%
-  group_by(country_code, name) %>%
+  select(-old_age_group, -name) %>%
+  rename(countrycode = country_code) %>%
+  group_by(countrycode) %>%
   summarize(ifr = sum(ifr * pop) / sum(pop)) %>%
   ungroup()
+
+# Subnational Mobility Data -----------------------------------------------
+
+subnat_mob_data <- haven::read_dta(file.path("~", "Dropbox", "COVID-19", "Data Collection", "Travel", "clean", "google_trends.dta")) %>%
+  select(countrycode_string, sub_region, date, starts_with("g_")) %>%
+  mutate_at(vars(starts_with("g_")), ~ . / 100) %>%
+  group_nest(countrycode_string, sub_region, .key = "daily_data") %>%
+  mutate(
+    first_day_mob = map(daily_data, pull, date) %>% map_dbl(min),
+    last_day_mob = map(daily_data, pull, date) %>% map_dbl(max),
+  ) %>%
+  mutate_at(vars(first_day_mob, last_day_mob), lubridate::as_date) %>%
+  mutate(
+    daily_data = pmap(lst(daily_data, first_day_mob, last_day_mob), ~ complete(..1, date = seq.Date(from = ..2, to = ..3, by = "day"))),
+    # Try to impute mobility. Doesn't work if variable starts with NAs
+    daily_data = map(daily_data, ~ tryCatch(mutate_at(.x, vars(starts_with("g_")), zoo::na.locf), error = function(err) .x)) %>%
+      map(mutate, average_mob = (g_grocery_pharma + g_parks + g_retail_recreation + g_workplaces) / 4)
+  )
+
+# Subnational Deaths Data -------------------------------------------------
+
+subnat_data <- haven::read_dta(file.path("~", "Dropbox", "COVID-19", "Analysis", "Data", "subspread.dta")) %>%
+  left_join(transmute(countrycode::codelist, countrycode_string = iso3c, countrycode = iso3n), by = "countrycode_string") %>% # Getting the countrycode
+  filter(!is.na(date)) %>%  # TODO this shouldn't happen in the clean data
+  group_nest(countrycode, countrycode_string, sub_region, .key = "daily_data") %>%
+  mutate(
+    first_observed_spread_day = map(daily_data, pull, date) %>%
+      map_dbl(min) %>%
+      lubridate::as_date(),
+
+    last_observed_spread_day = map(daily_data, pull, date) %>%
+      map_dbl(max) %>%
+      lubridate::as_date(),
+
+    first_deathgte10_day = map(daily_data, filter, cum_deaths >= min_deaths_day_before_epidemic) %>% # First day with >= 10 deaths
+      map_dbl(~ pull(.x, date) %>% min()) %>%
+      lubridate::as_date(),
+
+    first_infection_seeding_day = first_deathgte10_day - seeding_days_before_epidemic, # 30 days before the first day with >=10 deaths
+  ) %>%
+  filter(is.finite(first_deathgte10_day)) %>% # Only keep subnational units where cumulative reaches deaths >=10 (using is.finite() because is.na() doesn't work)
+  inner_join(subnat_mob_data, by = c("countrycode_string", "sub_region"), suffix = c("_spread", "_mob")) %>% # This restricts us to data with corresponding mobility datda
+  mutate(
+    last_effective_observed_day = min(last_observed_spread_day, last_day_mob), # We need both deaths and mobility day for the tail end of observed data (used in likelihood)
+    num_days_observed = last_effective_observed_day - first_infection_seeding_day + 1,
+
+    daily_data = map2(daily_data_spread, daily_data_mob, full_join, by = "date") %>%
+      list(first_infection_seeding_day, last_effective_observed_day) %>%
+      pmap(~ filter(..1, between(date, ..2, ..3))) %>%  # Keep only from start of seeding to last effective observed day
+      map2(first_infection_seeding_day, ~ complete(.x, date = seq.Date(from = .y, to = max(date), by = "day"))) %>% # Fill in missing days
+      map2(last_observed_spread_day,
+           ~ mutate(.x, new_deaths = if_else(date <= .y, coalesce(new_deaths, 0), new_deaths))) %>% # Convert NAs to zeroes, before last observed day
+      # TODO The below imputation of missing mobility outcomes needs work. Making simplifying assumptions.
+      map(mutate_at, vars(starts_with("g_")), zoo::na.locf, na.rm = FALSE) %>%  # Any non-leading NAs are replaced with last prior non-NA
+      map(mutate_at, vars(starts_with("g_")), zoo::na.locf, fromLast = TRUE, na.rm = FALSE) %>%  # Any leading NAs are replaced with first successor non-NA
+      map(mutate, average_mob = (g_grocery_pharma + g_parks + g_retail_recreation + g_workplaces) / 4), # Recalculate after handling NAs
+
+    num_missing_new_deaths = map(daily_data, filter, is.na(new_deaths)) %>% map_int(nrow),
+
+    num_missing_mob = map(daily_data, select, starts_with("g_")) %>% map(map, is.na) %>% map(reduce, ~ .x | .y) %>% map_int(sum)
+  ) %>%
+  select(-daily_data_mob, -daily_data_spread) %>%
+  arrange(countrycode, sub_region) %>%
+  left_join(wb_pop_data, by = c("countrycode_string", "sub_region")) %>% # Only available for regions (not all) in Brazil, India, and China
+  left_join(ifr_adj, by = "countrycode")
+
+use_subnat_data <- subnat_data %>%
+  filter(!is.na(pop)) %>%
+  arrange(countrycode)
 
 # Time to Death -----------------------------------------------------------
 
@@ -119,24 +163,74 @@ quant_time_to_death <- ecdf(
     EnvStats::rgammaAlt(1e6, 18.8, 0.45) # onset-to-death distribution
 )
 
-time_to_death <- c(0, seq(1.5, by = 1, length.out = max(subnat_deaths_data$days_observed))) %>%
+time_to_death <- c(0, seq(1.5, by = 1, length.out = max(use_subnat_data$num_days_observed) + days_to_forecast)) %>%
   map_dbl(quant_time_to_death) %>%
   subtract(lag(.)) %>%
   discard(is.na)
 
+# Plot timelines ----------------------------------------------------------
+
+use_subnat_data %>%
+  mutate(sub_region = fct_reorder(sub_region, first_deathgte10_day)) %>%
+  ggplot(aes(y = sub_region)) +
+  geom_errorbar(aes(xmin = first_observed_spread_day, xmax = last_observed_spread_day), width = 0.25) +
+  geom_linerange(aes(xmin = first_day_mob, xmax = last_day_mob), size = 7, alpha = 0.15) +
+  geom_point(aes(first_infection_seeding_day)) +
+  geom_point(aes(first_deathgte10_day + 1)) +
+  geom_point(aes(last_effective_observed_day)) +
+  ggrepel::geom_text_repel(aes(x = first_deathgte10_day + 1, label = "Epidemic"), nudge_y = 0.25) +
+  ggrepel::geom_text_repel(aes(x = first_infection_seeding_day, label = "First Seeding"), nudge_y = 0.25) +
+  ggrepel::geom_text_repel(aes(x = last_effective_observed_day, label = "Last Effective Observation"), nudge_y = 0.25) +
+  geom_linerange(aes(xmin = first_infection_seeding_day, xmax = first_deathgte10_day), linetype = "dashed") +
+  scale_color_discrete("") +
+  labs(x = "", y = "") +
+  facet_wrap(vars(countrycode_string), ncol = 1) +
+  theme_minimal()
+
 # Stan Data ---------------------------------------------------------------
 
-use_subnat_data <- subnat_data %>%
-  filter(!is.na(pop), days_observed > 0) %>%
-  arrange(countrycode)
+# use_subnat_data %<>% slice(1:2) # Testing with one unit
 
 stan_data <- lst(
   fit_model = 1,
 
   N_national = n_distinct(use_subnat_data$countrycode),
-  N_subnational = use_subnat_data %>% count(countrycode) %>% pull(n),
+  N_subnational = use_subnat_data %>% count(countrycode) %>% pull(n) %>% as.array(),
 
-  days_observed = use_subnat_data$days_observed,
-  days_to_impute_cases = 6,
+  start_epidemic_offset,
+  days_observed = as.integer(use_subnat_data$num_days_observed) %>% as.array(),
+  days_to_impute_cases = days_seeding,
+  days_to_forecast,
   total_days = max(days_observed),
+
+  time_to_death = time_to_death[1:total_days],
+
+  population = use_subnat_data$pop %>%
+    as.array(),
+  mean_ifr = use_subnat_data$ifr %>%
+    as.array(),
+
+  design_matrix = use_subnat_data %>%
+    unnest(daily_data) %>%
+    modelr::model_matrix(mob_formula),
+
+  num_coef = ncol(design_matrix),
+
+  deaths = use_subnat_data %>%
+    unnest(daily_data) %>%
+    pull(new_deaths),
 )
+
+# Sampling ----------------------------------------------------------------
+
+mob_model <- stan_model(file.path("mobility-model", "mobility.stan"))
+
+mob_fit <- mob_model %>%
+  sampling(
+    data = stan_data,
+    chains = 8,
+    iter = 4000,
+    control = lst(
+      adapt_delta = 0.9,
+    )
+  )
