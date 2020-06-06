6c66a0d1e51182d6f0afd976da3b47d088ea675c
Karim Naguib <karimn2.0@gmail.com>
2020-06-06 11:50:51 -0400

    Constants file

---
+++ mobility-model/constants.R
@@ -0,0 +1,6 @@
+min_deaths_day_before_epidemic <- 10
+seeding_days_before_epidemic <- 30
+start_epidemic_offset <- seeding_days_before_epidemic + 1
+days_seeding <- 6
+days_to_forecast <- 0
+mob_formula <- ~ 0 + g_residential + g_transit_stations + average_mob
