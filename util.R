7d1f0641c015e35821e51fb027d51bcebd3ba544
Karim Naguib <karimn2.0@gmail.com>
2020-06-16 21:22:34 -0400

    Plot data and allow plot different day estimates

--- util.R
+++ util.R
@@ -92,7 +92,28 @@ plot_subnat_ci <- function(results) {
     facet_wrap(vars(country_code), ncol = 1, scales = "free_y")
 }
 
-plot_day_ci <- function(results, use_date = FALSE) {
+plot_day_data <- function(results, par, use_date = FALSE) {
+  time_aes <- if (use_date) aes(x = date) else aes(x = day_index)
+  y_aes <- if (length(par) > 1) aes(y = value, color = name) else aes(y = value)
+
+  results %>%
+    select(country_code, sub_region, daily_data) %>%
+    unnest(daily_data) %>%
+    select(sub_region, day_index, date, all_of(par)) %>%
+    pivot_longer(par) %>%
+    ggplot(time_aes) +
+    geom_line(y_aes) +
+    scale_color_discrete("") +
+    labs(x = "", y = "") +
+    facet_wrap(vars(sub_region), ncol = 3, strip.position = "left") +
+    theme(
+      strip.placement = "outside",
+      strip.text = element_text(angle = 0),
+      axis.text.x = if (!use_date) element_blank()
+    )
+}
+
+plot_day_ci <- function(results, par, use_date = FALSE) {
   time_aes <- if (use_date) aes(x = date) else aes(x = day_index)
 
   results %>%
@@ -100,11 +121,10 @@ plot_day_ci <- function(results, use_date = FALSE) {
     unnest(daily_data) %>%
     select(country_code, sub_region, day_index, date, param_results) %>%
     unnest(param_results) %>%
-    filter(fct_match(parameter, "Rt_adj")) %>%
+    filter(fct_match(parameter, par)) %>%
     ggplot(time_aes) +
     geom_line(aes(y = per_0.5)) +
     geom_ribbon(aes(ymin = per_0.1, ymax = per_0.9), alpha = 0.25) +
-    geom_hline(yintercept = 1, linetype = "dotted") +
     labs(x = "", y = "") +
     facet_wrap(vars(sub_region), ncol = 3, strip.position = "left") +
     theme(
