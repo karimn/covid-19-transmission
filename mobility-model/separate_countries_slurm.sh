95afa38d5e6447fba2581f5c71fb1628daa0cbf5
Karim Naguib <karimn2.0@gmail.com>
2020-06-19 12:20:51 -0400

    Adjust options

--- mobility-model/separate_countries_slurm.sh
+++ mobility-model/separate_countries_slurm.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 #SBATCH -n 4                # Number of cores (-n)
 #SBATCH -N 1                # Ensure that all cores are on one Node (-N)
-#SBATCH -t 0-02:00          # Runtime in D-HH:MM, minimum of 10 minutes
-#SBATCH -p serial_requeue   # shared   # Partition to submit to
+#SBATCH -t 0-04:00          # Runtime in D-HH:MM, minimum of 10 minutes
+#SBATCH -p shared           # serial_requeue   # Partition to submit to
 #SBATCH --mem=8000 # 8GB    # Memory pool for all cores (see also --mem-per-cpu)
-#SBATCH -o ../temp/log/mob_%A_%a.out  # File to which STDOUT will be written, %j inserts jobid
-#SBATCH -e ../temp/log/mob_%A_%a.err  # File to which STDERR will be written, %j inserts jobid
+#SBATCH -o ../temp/log/mob_%A_%a.log  # File to which STDOUT will be written, %j inserts jobid
+#SBATCH -e ../temp/log/mob_%A_%a.log  # File to which STDERR will be written, %j inserts jobid
 
 module load gcc/9.2.0-fasrc01 R_core/3.6.3-fasrc01
 
@@ -15,5 +15,5 @@ module load gcc/9.2.0-fasrc01 R_core/3.6.3-fasrc01
 #  mkdir "../temp/log/job_%A"
 #fi
 
-Rscript run_mob.R fit ${SLURM_ARRAY_TASK_ID} -i 2000 -o "{all_country_codes}_${SLURM_JOB_ID}_${SLURM_ARRAY_TASK_ID}_mob" --hyperparam=separate_hyperparam.yaml --show-script-options
+Rscript run_mob.R fit ${SLURM_ARRAY_TASK_ID} -i 2000 -o "{all_country_codes}_${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}_mob" --hyperparam=separate_hyperparam.yaml --show-script-options
 
