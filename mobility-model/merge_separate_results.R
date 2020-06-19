630a9397aba3ba99403f1109cdbc77a93d4c3e98
Karim Naguib <karimn2.0@gmail.com>
2020-06-19 12:58:14 -0400

    Merge results from separate runs

---
+++ mobility-model/merge_separate_results.R
@@ -0,0 +1,28 @@
+#!/bin/Rscript
+
+"Usage:
+  merge_separate_results <location> <destination>
+" -> opt_desc
+
+library(magrittr)
+library(tidyverse)
+
+if (interactive()) {
+  root_path <- "."
+
+  docopt::docopt(opt_desc, "")
+} else {
+  root_path <- ".."
+
+  setwd(root_path)
+  source(file.path("renv", "activate.R"))
+  setwd("mobility-model")
+
+  docopt::docopt(opt_desc)
+}
+
+merged_results <- dir(script_options$location, pattern = "\\w{2}_\\d+_\\d+_mob_results\\.rds$", full.names = TRUE) %>%
+  map_dfr(read_rds)
+
+write_rds(merged_results, script_options$destination)
+
