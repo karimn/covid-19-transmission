93667b1c2b7f4981dfd0c299f14bb38dcef945df
Karim Naguib <karimn2.0@gmail.com>
2020-06-20 14:33:07 -0400

    Increase memory

--- mobility-model/separate_us_slurm.sh
+++ mobility-model/separate_us_slurm.sh
@@ -2,15 +2,15 @@
 
 #SBATCH -n 4                # Number of cores (-n)
 #SBATCH -N 1                # Ensure that all cores are on one Node (-N)
-#SBATCH -t 0-16:00          # Runtime in D-HH:MM, minimum of 10 minutes
+#SBATCH -t 0-14:00          # Runtime in D-HH:MM, minimum of 10 minutes
 #SBATCH -p shared           # serial_requeue   # Partition to submit to
-#SBATCH --mem=8000 # 8GB    # Memory pool for all cores (see also --mem-per-cpu)
-#SBATCH -o ../temp/log/mob_%A_%a.log  # File to which STDOUT will be written, %j inserts jobid
-#SBATCH -e ../temp/log/mob_%A_%a.log  # File to which STDERR will be written, %j inserts jobid
+#SBATCH --mem=16000 # 16GB    # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH -o ../temp/log/mob_us_%j.log  # File to which STDOUT will be written, %j inserts jobid
+#SBATCH -e ../temp/log/mob_us_%j.log  # File to which STDERR will be written, %j inserts jobid
 
 module load gcc/9.2.0-fasrc01 R_core/3.6.3-fasrc01
 
 RUN_SUFFIX=mob
 
-Rscript run_mob.R fit us -i 4000 -o "us_${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}_${RUN_SUFFIX}" --hyperparam=separate_hyperparam.yaml --show-script-options
+Rscript run_mob.R fit us -i 4000 -o "us_${SLURM_JOB_ID}_${RUN_SUFFIX}" --hyperparam=separate_hyperparam.yaml --show-script-options
 
