cfb01536ddffb83a567759383fb9eb6ce3a0e725
Karim Naguib <karimn2.0@gmail.com>
2020-06-20 16:06:27 -0400

    Append diagnostics

--- mobility-model/merge_separate_results.R
+++ mobility-model/merge_separate_results.R
@@ -1,7 +1,7 @@
 #!/bin/Rscript
 
 "Usage:
-  merge_separate_results <location> <destination>
+  merge_separate_results <location> <destination> <diagnostics> [--lite]
 " -> opt_desc
 
 library(magrittr)
@@ -21,8 +21,23 @@ script_options <- if (interactive()) {
   docopt::docopt(opt_desc)
 }
 
+diagnostics_data <- read_tsv(script_options$diagnostics, col_names = c("job_id", "country_index", "divergent_trans", "max_rhat", "min_ess_bulk", "min_ess_tail"))
+
 merged_results <- dir(script_options$location, pattern = "\\w{2}_\\d+_\\d+_mob_results\\.rds$", full.names = TRUE) %>%
-  map_dfr(read_rds)
+  map_dfr({
+    tibble(
+      results_file = .x,
+      results = list(read_rds(.x))
+    )
+  }) %>%
+  tidyr::extract(results_file, "job_id", "\\w{2}_\\d+_(\\d+)", remove = FALSE) %>%
+  left_join(diagnostics_data, by = c("job_id", "country_index")) %>%
+  unnest(results)
+
+if (script_options$lite) {
+  merged_results %<>%
+    trim_iter_data()
+}
 
 write_rds(merged_results, script_options$destination)
 
