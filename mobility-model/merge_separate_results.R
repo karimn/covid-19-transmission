6d77f00290128f85273a3f1e40d2270edd66b8ee
Karim Naguib <karimn2.0@gmail.com>
2020-06-20 22:16:36 -0400

    Fix regex

--- mobility-model/merge_separate_results.R
+++ mobility-model/merge_separate_results.R
@@ -10,7 +10,7 @@ Options:
 script_options <- if (interactive()) {
   root_path <- "."
 
-  docopt::docopt(opt_desc, "../data/mobility/results/clean_epi3deaths/ ../data/mobility/results/lite_merged_epi3deaths.rds ../temp/log/diag_61500659.tsv --lite")
+  docopt::docopt(opt_desc, "../data/mobility/results/clean_epi3deaths/ ../data/mobility/results/lite_merged_epi3deaths.rds ../temp/log/diag_61500659.tsv --lite --run-suffix=epi3deaths_mob")
 } else {
   root_path <- ".."
 
@@ -26,7 +26,7 @@ library(tidyverse)
 
 diagnostics_data <- read_tsv(script_options$diagnostics, col_names = c("job_id", "country_index", "divergent_trans", "max_rhat", "min_ess_bulk", "min_ess_tail"))
 
-merged_results <- dir(script_options$location, pattern = str_glue("\\w{2}_\\d+_\\d+_{script_options$`run-suffix`}_results\\.rds$"), full.names = TRUE) %>%
+merged_results <- dir(script_options$location, pattern = str_glue("\\w{{2}}_\\d+_\\d+_{script_options$`run-suffix`}_results\\.rds$"), full.names = TRUE) %>%
   map_dfr(~ tibble(
     results_file = .x,
     results = list(read_rds(.x))
