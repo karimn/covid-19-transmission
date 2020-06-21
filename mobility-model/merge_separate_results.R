024e17d332956354d5c2f4acce40e06bc99da71a
Karim Naguib <karimn2.0@gmail.com>
2020-06-20 22:31:14 -0400

    Set types

--- mobility-model/merge_separate_results.R
+++ mobility-model/merge_separate_results.R
@@ -24,14 +24,16 @@ script_options <- if (interactive()) {
 library(magrittr)
 library(tidyverse)
 
-diagnostics_data <- read_tsv(script_options$diagnostics, col_names = c("job_id", "country_index", "divergent_trans", "max_rhat", "min_ess_bulk", "min_ess_tail"))
+diagnostics_data <- read_tsv(script_options$diagnostics,
+                             col_names = c("job_id", "country_index", "divergent_trans", "max_rhat", "min_ess_bulk", "min_ess_tail"),
+                             col_types = cols(job_id = col_integer(), country_index = col_integer()))
 
 merged_results <- dir(script_options$location, pattern = str_glue("\\w{{2}}_\\d+_\\d+_{script_options$`run-suffix`}_results\\.rds$"), full.names = TRUE) %>%
   map_dfr(~ tibble(
     results_file = .x,
     results = list(read_rds(.x))
   )) %>%
-  tidyr::extract(results_file, "job_id", "\\w{2}_\\d+_(\\d+)", remove = FALSE) %>%
+  tidyr::extract(results_file, "job_id", "\\w{2}_\\d+_(\\d+)", remove = FALSE, convert = TRUE) %>%
   unnest(results) %>%
   left_join(diagnostics_data, by = c("job_id", "country_index"))
 
