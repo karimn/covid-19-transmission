52a76659f198aa69eff190e35bac1b9e1fade490
Karim Naguib <karimn2.0@gmail.com>
2020-06-20 22:47:44 -0400

    Get actual job id!

--- mobility-model/merge_separate_results.R
+++ mobility-model/merge_separate_results.R
@@ -35,7 +35,7 @@ merged_results <- dir(script_options$location, pattern = str_glue("\\w{{2}}_\\d+
     results_file = .x,
     results = list(read_rds(.x))
   )) %>%
-  tidyr::extract(results_file, "job_id", "\\w{2}_\\d+_(\\d+)", remove = FALSE, convert = TRUE) %>%
+  tidyr::extract(results_file, "job_id", "\\w{2}_(\\d+)_\\d+", remove = FALSE, convert = TRUE) %>%
   unnest(results) %>%
   left_join(diagnostics_data, by = c("job_id", "country_index"))
 
