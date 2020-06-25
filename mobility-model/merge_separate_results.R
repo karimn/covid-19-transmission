68fe9899d9ae8a49375da050b805017150047f5e
Karim Naguib <karimn2.0@gmail.com>
2020-06-25 11:36:27 -0400

    Fix reading diagnostics

--- mobility-model/merge_separate_results.R
+++ mobility-model/merge_separate_results.R
@@ -27,7 +27,7 @@ library(tidyverse)
 source(file.path(root_path, "mobility-model", "mob_util.R"))
 
 diagnostics_data <- read_tsv(script_options$diagnostics,
-                             col_names = c("job_id", "country_index", "divergent_trans", "max_rhat", "min_ess_bulk", "min_ess_tail"),
+                             col_names = c("job_id", "country_index", "divergent_trans", "low_bfmi", "max_rhat", "min_ess_bulk", "min_ess_tail", "iter1", "iter2", "iter3", "iter4"),
                              col_types = cols(job_id = col_integer(), country_index = col_integer()))
 
 merged_results <- dir(script_options$location, pattern = str_glue("\\w{{2}}_\\d+_\\d+_{script_options$`run-suffix`}_results\\.rds$"), full.names = TRUE) %>%
