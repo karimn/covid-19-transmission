b97590a21f0d1538094ed67fb2f61aaf18bf6685
Karim Naguib <karimn2.0@gmail.com>
2020-06-26 13:22:33 -0400

    Fix cmdline arg

--- mobility-model/separate_countries_slurm.sh
+++ mobility-model/separate_countries_slurm.sh
@@ -13,5 +13,5 @@ module load gcc/9.2.0-fasrc01 R_core/3.6.3-fasrc01
 RUN_SUFFIX=$2
 OUTPUT_ARG="-o {all_country_codes}_${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}_${RUN_SUFFIX} --output-dir=${SCRATCH}/kremer_lab/karimn/mob_results"
 
-Rscript run_mob.R $1 ${SLURM_ARRAY_TASK_ID} -i $3 --hyperparam=separate_hyperparam.yaml --show-script-options $OUTPUT_ARGS --use-param-trend
+Rscript run_mob.R $1 ${SLURM_ARRAY_TASK_ID} -i $3 --hyperparam=separate_hyperparam.yaml --show-script-options $OUTPUT_ARGS --include-param-trend
 
