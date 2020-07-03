e0306e8e6fa81c0854deae701f7de1c285ec1238
Karim Naguib <karimn2.0@gmail.com>
2020-07-03 11:00:40 -0400

    Latest run args

--- mobility-model/separate_countries_slurm.sh
+++ mobility-model/separate_countries_slurm.sh
@@ -10,9 +10,12 @@
 
 module load gcc/9.2.0-fasrc01 R_core/3.6.3-fasrc01
 
-RUN_SUFFIX=$2
-OUTPUT_ARGS="-o {all_country_codes}_${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}_${RUN_SUFFIX} --output-dir=${SCRATCH}/kremer_lab/karimn/mob_results/run_${SLURM_ARRAY_JOB_ID}"
-HIERARCH_ARGS="--no-partial-pooling=trend --center-mobility-model --center-log-r0"
-MOB_MODEL_TYPE="--mobility-model-type=${4}"
+run_type=$1
+run_suffix=$2
+iter=$3
+output_args="-o {all_country_codes}_${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}_${run_suffix} --output-dir=${SCRATCH}/kremer_lab/karimn/mob_results/run_${SLURM_ARRAY_JOB_ID}"
+#hierarch_args="--no-partial-pooling=trend --center-mobility-model --center-log-r0"
+hierarch_args="--no-partial-pooling=trend"
+mob_model_type="--mobility-model-type=${4}"
 
-Rscript run_mob.R $1 ${SLURM_ARRAY_TASK_ID} -i $3 --hyperparam=separate_hyperparam.yaml --show-script-options --include-param-trend $OUTPUT_ARGS $HIERARCH_ARGS $MOB_MODEL_TYPE
+Rscript run_mob.R ${run_type} ${SLURM_ARRAY_TASK_ID} -i ${iter} --hyperparam=separate_hyperparam.yaml --show-script-options --include-param-trend $output_args $hierarch_args $mob_model_type
