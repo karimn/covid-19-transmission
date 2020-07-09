9fbf5fd5e4e1f2eaa48e4a5dc4dad28e14f16e98
Karim Naguib <karimn2.0@gmail.com>
2020-07-09 10:21:08 -0400

    Increase adapt-delta to 0.99

--- mobility-model/run_mob.R
+++ mobility-model/run_mob.R
@@ -463,7 +463,7 @@ if (script_options$cmdstan) {
     iter = script_options$iter,
     chains = script_options$chains,
     control = lst(
-      adapt_delta = 0.95,
+      adapt_delta = 0.99,
       max_treedepth = 12
     ),
     init = if (script_options$`random-init`) "random" else make_initializer(stan_data),
