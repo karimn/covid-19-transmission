ca49abfb8effb85fcce723c219af77c5f4761288
Karim Naguib <karimn2.0@gmail.com>
2020-07-10 22:11:08 -0400

    Testing commands

--- mobility-model/diagnose_divtran.R
+++ mobility-model/diagnose_divtran.R
@@ -4,10 +4,17 @@ library(rstan)
 library(bayesplot)
 library(cowplot)
 
+color_scheme_set("darkgray")
+
 source(file.path("util.R"))
 source(file.path("mobility-model", "mob_util.R"))
 
-load("data/mobility/results/pk_62930480_97_mob.RData")
+# load("data/mobility/results/pk_63034482_97_mob.RData")
+# load("data/mobility/results/ca_63034482_23_mob.RData")
+# load("data/mobility/results/ca_63036887_23_mob.RData")
+# load("data/mobility/results/pk_63036887_97_mob.RData")
+# load("temp/pk_63120140_97_mob.RData")
+load("temp/pk_63062564_97_mob.RData") # Old uncentered
 
 check_hmc_diagnostics(mob_fit)
 
@@ -22,8 +29,8 @@ my_lp <- log_posterior(mob_fit) %>%
 my_np <- nuts_params(mob_fit) %>%
   filter(Iteration %in% sub_iter)
 
-my_all_parameters <- extract_parameters(mob_fit) %>%
-  select(-iter_data)
+# my_all_parameters <- extract_parameters(mob_fit) %>%
+#   select(-iter_data)
 
 # load("data/mobility/results/co_62090526_27_mob.RData")
 #
@@ -36,32 +43,31 @@ my_all_parameters <- extract_parameters(mob_fit) %>%
 # co_all_parameters <- extract_parameters(mob_fit) %>%
 #   select(-iter_data)
 
-color_scheme_set("darkgray")
-
-# mcmc_parcoord(my_posterior, np = my_np, pars = c("overdisp_deaths", "tau_impute_cases", "mean_deaths[1075]"))
 mcmc_parcoord(my_posterior, np = my_np, pars = c("overdisp_deaths", "mean_deaths[10]", # "imputed_cases[2]",
-                                                 "toplevel_log_R0", "subnational_effect_log_R0_raw[1]", "subnational_effect_log_R0[1]", "subnational_effect_log_R0_sd[1]",
+                                                 "toplevel_log_R0", "subnational_log_R0[1]", "subnational_log_R0_sd[1]",
                                                  "beta_toplevel[1]", "beta_toplevel[2]", "beta_toplevel[3]",
-                                                 "beta_subnational_raw[2,1]", "beta_subnational_sd[2,1]"),
-              alpha = 0.1, np_style = parcoord_style_np(div_alpha = 1, div_size = 0.5))
-
-plot_grid(
-  mcmc_parcoord(co_posterior, np = co_np, pars = c("overdisp_deaths", "tau_impute_cases", "mean_deaths[1075]"))
-)
+                                                 "beta_subnational[2,1]", "beta_subnational_sd[2,1]"))
+              # alpha = 0.1, np_style = parcoord_style_np(div_alpha = 1, div_size = 0.5))
 
 mcmc_pairs(
   my_posterior, np = my_np,
-  pars = c("overdisp_deaths", "mean_deaths[10]", "imputed_cases[2]",
-           "toplevel_log_R0", "subnational_effect_log_R0_raw[1]", "subnational_effect_log_R0[1]", "subnational_effect_log_R0_sd[1]",
-           "beta_toplevel[2]", "beta_subnational_raw[2,1]", "beta_subnational_sd[2,1]",
+  pars = c("overdisp_deaths", "mean_deaths[10]", "imputed_cases[1]",
+           # "toplevel_log_R0", "subnational_log_R0[1]", "subnational_log_R0_sd[1]",
+           # "beta_toplevel[2]", "beta_subnational[2,1]", "beta_subnational_sd[2,1]",
+           "toplevel_log_R0", #"subnational_effect_log_R0_raw[1]", "subnational_effect_log_R0_sd[1]",
+           "beta_toplevel[2]", #"beta_subnational_raw[2,1]", "beta_subnational_sd[2,1]",
            "ifr_noise[1]",
            "trend_lambda[1]", "toplevel_trend_kappa"),
-  transformations = lst(overdisp_deaths = "log", "mean_deaths[10]" = "log", "subnational_effect_log_R0_sd[1]" = "log",
-                        "beta_subnational_sd[2,1]" = "log", "imputed_cases[2]" = "log",
+  transformations = lst(overdisp_deaths = "log",
+                        "mean_deaths[10]" = "log",
+                        # "subnational_log_R0_sd[1]" = "log",
+                        # "subnational_effect_log_R0_sd[1]" = "log",
+                        # "beta_subnational_sd[2,1]" = "log",
+                        "imputed_cases[1]" = "log",
                         "trend_lambda[1]" = "log", "toplevel_trend_kappa" = function(kappa) log(-kappa),
                         "ifr_noise[1]" = "log"),
-  off_diag_args = list(size = 0.75),
-  np_style = pairs_style_np(div_size = 2, div_shape = 16, td_alpha = 0.1)
+  off_diag_args = list(size = 0.75)
+  # np_style = pairs_style_np(div_size = 2, div_shape = 16, td_alpha = 0.1)
 )
 
 mcmc_pairs(my_posterior, np = my_np, pars = c("overdisp_deaths",
