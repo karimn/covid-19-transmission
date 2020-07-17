cd1fa3bdbcf88dd51d0355f13f3615856d445160
Karim Naguib <karimn2.0@gmail.com>
2020-07-16 20:52:57 -0400

    parser takes range of jobs ids now

--- mobility-model/slurm_tools/msing_log_parser.sh
+++ mobility-model/slurm_tools/msing_log_parser.sh
@@ -1,10 +1,14 @@
 #/usr/bin/env bash
 
-#printf "job_id\ttask_id\tdiv_tran\tlow_bfmi\tRhat\tess_bulk\tess_tail\titer1\titer2\titer3\titer4\tstatus\tcountry_name\n"
-
 slurm_tools_dir=`dirname $0`
+first_job_id=$1
+last_job_id=$2
 
-for f in $* 
-do 
-  ${slurm_tools_dir}/log_parser.awk mob_$f.log
+for f in `seq ${first_job_id} ${last_job_id}`
+do
+  log_file=mob_$f.log
+  if [ -e $log_file ]
+  then
+    ${slurm_tools_dir}/log_parser.awk $log_file
+  fi
 done
