0ff5c52abebb445ec26fcdbf167cdf4019751049
Karim Naguib <karimn2.0@gmail.com>
2020-07-27 11:20:27 -0400

    Updating to include different results (separate and joint)

--- mobility-model/merge_separate_results.R
+++ mobility-model/merge_separate_results.R
@@ -1,16 +1,17 @@
 #!/bin/Rscript
 
 "Usage:
-  merge_separate_results <location> <destination> <diagnostics> [--lite --run-suffix=<suffix>]
+  merge_separate_results <location> <destination> [--lite --results-file-pattern=<pattern>]
 
 Options:
-  --run-suffix=<suffix>  Run suffix used in results files [default: mob]
+  --lite  Drop all sampling iterations
+  --results-file-pattern=<pattern>  Pattern used to find results .rds files [default: _results\\.rds$]
 " -> opt_desc
 
 script_options <- if (interactive()) {
   root_path <- "."
 
-  docopt::docopt(opt_desc, "../data/mobility/results/clean_epi3deaths/ ../data/mobility/results/lite_merged_epi3deaths.rds ../temp/log/diag_61500659.tsv --lite --run-suffix=epi3deaths_mob")
+  docopt::docopt(opt_desc, "../data/mobility/results/clean_epi3deaths/ ../data/mobility/results/lite_merged_epi3deaths.rds --lite")
 } else {
   root_path <- ".."
 
@@ -26,19 +27,20 @@ library(tidyverse)
 
 source(file.path(root_path, "mobility-model", "mob_util.R"))
 
-diagnostics_data <- read_tsv(script_options$diagnostics,
-                             col_names = c("job_id", "country_index", "country_code", "divergent_trans", "low_bfmi", "max_rhat", "min_ess_bulk", "min_ess_tail", "iter1", "iter2", "iter3", "iter4", "status", "time", "country_name"),
-                             col_types = cols(job_id = col_integer(), country_index = col_integer())) %>%
-  select(-status, -country_name, -max_rhat, -starts_with("min_ess"))
+# diagnostics_data <- read_tsv(script_options$diagnostics,
+#                              col_names = c("job_id", "country_index", "country_code", "divergent_trans", "low_bfmi", "max_rhat", "min_ess_bulk", "min_ess_tail", "iter1", "iter2", "iter3", "iter4", "status", "time", "country_name"),
+#                              col_types = cols(job_id = col_integer(), country_index = col_integer())) %>%
+#   select(-status, -country_name, -max_rhat, -starts_with("min_ess"))
 
-merged_results <- dir(script_options$location, pattern = str_glue("\\w{{2}}_\\d+_\\d+_{script_options$`run-suffix`}_results\\.rds$"), full.names = TRUE) %>%
-  map_dfr(~ tibble(
-    results_file = .x,
-    results = list(read_rds(.x))
-  )) %>%
-  tidyr::extract(results_file, "job_id", "\\w{2}_(\\d+)_\\d+", remove = FALSE, convert = TRUE) %>%
-  unnest(results) %>%
-  left_join(diagnostics_data, by = c("job_id", "country_index", "country_code"))
+merged_results <- dir(script_options$location, pattern = script_options$`results-file-pattern`, full.names = TRUE) %>%
+  # map_dfr(~ tibble(
+  #   results_file = .x,
+  #   results = list(read_rds(.x))
+  # )) %>%
+  map_dfr(~ read_rds(.x) %>% mutate(results_file = .x)) %>%
+  # tidyr::extract(results_file, "job_id", "\\w{2}_(\\d+)_\\d+", remove = FALSE, convert = TRUE) %>%
+  unnest(results)
+  # left_join(diagnostics_data, by = c("job_id", "country_index", "country_code"))
 
 if (script_options$lite) {
   merged_results %<>%
