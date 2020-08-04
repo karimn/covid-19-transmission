0192545ce84aa08032f352dba6e7abef43961d8e
Karim Naguib <karimn2.0@gmail.com>
2020-08-04 16:17:31 -0400

    Remove old code

--- mobility-model/merge_results.R
+++ mobility-model/merge_results.R
@@ -28,20 +28,8 @@ library(tidyverse)
 
 source(file.path(root_path, "mobility-model", "mob_util.R"))
 
-# diagnostics_data <- read_tsv(script_options$diagnostics,
-#                              col_names = c("job_id", "country_index", "country_code", "divergent_trans", "low_bfmi", "max_rhat", "min_ess_bulk", "min_ess_tail", "iter1", "iter2", "iter3", "iter4", "status", "time", "country_name"),
-#                              col_types = cols(job_id = col_integer(), country_index = col_integer())) %>%
-#   select(-status, -country_name, -max_rhat, -starts_with("min_ess"))
-
 merged_results <- dir(script_options$location, pattern = script_options$`results-file-pattern`, full.names = TRUE) %>%
-  # map_dfr(~ tibble(
-  #   results_file = .x,
-  #   results = list(read_rds(.x))
-  # )) %>%
   map_dfr(~ read_rds(.x) %>% mutate(results_file = .x))
-  # tidyr::extract(results_file, "job_id", "\\w{2}_(\\d+)_\\d+", remove = FALSE, convert = TRUE) %>%
-  # unnest(results)
-  # left_join(diagnostics_data, by = c("job_id", "country_index", "country_code"))
 
 if (script_options$lite) {
   merged_results %<>%
